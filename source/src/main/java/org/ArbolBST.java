3ce84a6b3b2fa2e65c49532d076325c9111bfa78
elkinalzatel-rgb <elkin.alzatel@autonoma.edu.co>
2026-05-26 19:22:19 -0500

    se añaden los atributos de la clase ArbolBST, y se crean sus métodos (sin lógica)

--- source/src/main/java/org/ArbolBST.java
+++ source/src/main/java/org/ArbolBST.java
@@ -1,4 +1,52 @@
 package org;
 
 public class ArbolBST {
+    private Nodo raiz;
+    private int height;
+    private int totalTeams;
+
+    public ArbolBST() {
+        this.raiz = null;
+        this.height = 0;
+        this.totalTeams = 0;
+    }
+
+    public void insert() {
+    }
+
+    public void delete() {
+    }
+
+    public void search() {
+    }
+
+    public void searchFavorite() {
+    }
+
+    public void searchWeak() {
+    }
+
+    public void printRanking() {
+    }
+
+    public void export() {
+    }
+
+    public void printGoalDiff() {
+    }
+
+    public void viewTree() {
+    }
+
+    public Nodo getRaiz() {
+        return raiz;
+    }
+
+    public int getHeight() {
+        return height;
+    }
+
+    public int getTotalTeams() {
+        return totalTeams;
+    }
 }
