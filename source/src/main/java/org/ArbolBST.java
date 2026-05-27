b570a683a0b435fd8d14d33c510471e1d1440863
elkinalzatel-rgb <elkin.alzatel@autonoma.edu.co>
2026-05-27 16:59:49 -0500

    se crea la lógica de los métodos propios de un arbol binario de busqueda (insert, delete y search)

--- source/src/main/java/org/ArbolBST.java
+++ source/src/main/java/org/ArbolBST.java
@@ -11,14 +11,86 @@ public class ArbolBST {
         this.totalTeams = 0;
     }
 
-    public void insert(Nodo nodo, int ranking) {
-        if (raiz == null) raiz = nodo;
+    public void insert(Seleccion seleccion) {
+        raiz = insertNewNode(raiz, seleccion);
+        totalTeams++;
     }
 
-    public void delete() {
+    private Nodo insertNewNode(Nodo nodo, Seleccion seleccion) {
+        if (nodo == null) {
+            return new Nodo(seleccion);
+        }
+        int rankingActual = nodo.getSeleccion().getRankingFIFA();
+        int rankingNuevo = seleccion.getRankingFIFA();
+
+        if (rankingNuevo < rankingActual) {
+            nodo.setIzquierda(insertNewNode(nodo.getIzquierda(), seleccion));
+            height++;
+        } else if (rankingNuevo > rankingActual) {
+            nodo.setDerecha(insertNewNode(nodo.getDerecha(), seleccion));
+            height++;
+        }
+        return nodo;
     }
 
-    public void search() {
+    public void delete(int rankingFIFA) {
+        raiz = deleteNode(raiz, rankingFIFA);
+        totalTeams--;
+    }
+
+    private Nodo deleteNode(Nodo nodo, int rankingFIFA) {
+        if (nodo == null) {
+            return null;
+        }
+        int rankingActual = nodo.getSeleccion().getRankingFIFA();
+
+        if (rankingFIFA < rankingActual) {
+            nodo.setIzquierda(deleteNode(nodo.getIzquierda(), rankingFIFA));
+        } else if (rankingFIFA > rankingActual) {
+            nodo.setDerecha(deleteNode(nodo.getDerecha(), rankingFIFA));
+        } else {
+            if (nodo.getIzquierda() == null && nodo.getDerecha() == null) {
+                return null;
+            }
+            if (nodo.getIzquierda() == null) {
+                return nodo.getDerecha();
+            }
+            if (nodo.getDerecha() == null) {
+                return nodo.getIzquierda();
+            }
+            Nodo sucesor = findMin(nodo.getDerecha());
+            nodo.setSeleccion(sucesor.getSeleccion());
+            nodo.setDerecha(deleteNode(nodo.getDerecha(), sucesor.getSeleccion().getRankingFIFA()));
+        }
+
+        return nodo;
+    }
+
+    private Nodo findMin(Nodo nodo) {
+        while (nodo.getIzquierda() != null) {
+            nodo = nodo.getIzquierda();
+        }
+        return nodo;
+    }
+
+    public Seleccion search(int rankingFIFA) {
+        return searchNode(raiz, rankingFIFA);
+    }
+
+    private Seleccion searchNode(Nodo nodo, int rankingFIFA) {
+        if (nodo == null) {
+            return null;
+        }
+
+        int rankingActual = nodo.getSeleccion().getRankingFIFA();
+
+        if (rankingFIFA < rankingActual) {
+            return searchNode(nodo.getIzquierda(), rankingFIFA);
+        } else if (rankingFIFA > rankingActual) {
+            return searchNode(nodo.getDerecha(), rankingFIFA);
+        } else {
+            return nodo.getSeleccion();
+        }
     }
 
     public void searchFavorite() {
--- source/src/main/java/org/Main.java
+++ source/src/main/java/org/Main.java
@@ -1,4 +1,7 @@
 package main.java.org;
 
 public class Main {
+    public static void main(String[] args) {
+        
+    }
 }
