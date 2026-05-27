9379d5de59c49f82d68e14d5efe55c0c7cb5fe98
jhoem <jhoemmathias@gmail.com>
2026-05-27 12:26:53 -0500

    clase Nodo fix

--- source/src/main/java/org/Nodo.java
+++ source/src/main/java/org/Nodo.java
@@ -1,4 +1,41 @@
 package main.java.org;
 
+package mundial;
+
 public class Nodo {
+
+    private Seleccion seleccion;
+    private Nodo izquierda;
+    private Nodo derecha;
+
+    public Nodo(Seleccion seleccion) {
+        this.seleccion = seleccion;
+        this.izquierda = null;
+        this.derecha = null;
+    }
+
+
+    public Seleccion getSeleccion() {
+        return seleccion;
+    }
+
+    public void setSeleccion(Seleccion seleccion) {
+        this.seleccion = seleccion;
+    }
+
+    public Nodo getIzquierda() {
+        return izquierda;
+    }
+
+    public void setIzquierda(Nodo izquierda) {
+        this.izquierda = izquierda;
+    }
+
+    public Nodo getDerecha() {
+        return derecha;
+    }
+
+    public void setDerecha(Nodo derecha) {
+        this.derecha = derecha;
+    }
 }
